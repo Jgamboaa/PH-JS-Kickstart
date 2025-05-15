35e3ca6fd88629f60a377d20c6bfa26609c40cac
Jgamboaa <jgamboaa@miumg.edu.gt>
2025-05-15 16:03:30 -0600

    Agregar campos para habilitar la autenticación de dos factores en la tabla admin

--- config/2fa_update.sql
+++ config/2fa_update.sql
@@ -1,5 +1,3 @@
--- filepath: c:\laragon\www\PH-JS-Kickstart\config\2fa_update.sql
--- Agregar campos para autenticación de dos factores
 ALTER TABLE `admin` 
 ADD COLUMN `tfa_secret` VARCHAR(100) NULL AFTER `admin_estado`,
 ADD COLUMN `tfa_enabled` TINYINT(1) NOT NULL DEFAULT 0 AFTER `tfa_secret`,
