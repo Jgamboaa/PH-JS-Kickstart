c49b2e969b5007a35abf837175dc9e8915fcffde
Jgamboaa <jgamboaa@miumg.edu.gt>
2025-06-27 16:48:13 -0600

    Refactor la función renderModal para mejorar la gestión de opciones y atributos, añadiendo soporte para nuevas configuraciones como 'centered' y 'scrollable', y optimizando la construcción del HTML.

--- admin/components/modal.php
+++ admin/components/modal.php
@@ -1,34 +1,88 @@
 <?php
-// admin/includes/components/modal.php
-function renderModal($options = [])
+// admin/includes/components/form_fields.php
+
+function renderModal(array $options = [])
 {
+    // 1. Defaults
     $defaults = [
-        'id' => 'genericModal',
-        'title' => 'Modal',
-        'size' => '', // 'modal-lg', 'modal-xl', 'modal-sm'
-        'scrollable' => false,
-        'body' => '',
-        'footer' => '',
-        'closable' => true
+        'id'             => 'genericModal',
+        'title'          => 'Modal Title',
+        'size'           => '',      // '', 'modal-sm', 'modal-lg', 'modal-xl'
+        'scrollable'     => false,   // modal-dialog-scrollable
+        'centered'       => false,   // modal-dialog-centered
+        'backdrop'       => true,    // true, false, 'static'
+        'keyboard'       => true,    // close on ESC
+        'show'           => false,   // whether to add 'show' class initially
+        'closable'       => true,    // show close button
+        'body'           => '',      // HTML string
+        'footer'         => '',      // HTML string
+        'headerClass'    => '',      // extra classes for .modal-header
+        'bodyClass'      => '',      // extra classes for .modal-body
+        'footerClass'    => '',      // extra classes for .modal-footer
     ];
+    $cfg = array_merge($defaults, $options);
 
-    $config = array_merge($defaults, $options);
-    $sizeClass = $config['size'] ? $config['size'] : '';
-    $scrollableClass = $config['scrollable'] ? 'modal-dialog-scrollable' : '';
+    // 2. Compute attributes & classes
+    $dialogClasses = trim(sprintf(
+        "%s %s %s",
+        $cfg['size'],
+        $cfg['scrollable'] ? 'modal-dialog-scrollable' : '',
+        $cfg['centered']   ? 'modal-dialog-centered'    : ''
+    ));
+    $backdropAttr = '';
+    if ($cfg['backdrop'] === false)
+    {
+        $backdropAttr = 'data-backdrop="false"';
+    }
+    elseif ($cfg['backdrop'] === 'static')
+    {
+        $backdropAttr = 'data-backdrop="static"';
+    }
+    $keyboardAttr = $cfg['keyboard'] ? '' : 'data-keyboard="false"';
+    $showClass = $cfg['show'] ? ' show' : '';
+    $ariaLabelledby = $cfg['id'] . 'Label';
 
-    return "
-    <div class='modal fade' id='{$config['id']}' tabindex='-1'>
-        <div class='modal-dialog {$sizeClass} {$scrollableClass}'>
-            <div class='modal-content'>
-                <div class='modal-header'>
-                    <h5 class='modal-title'>{$config['title']}</h5>
-                    " . ($config['closable'] ? "<button type='button' class='close' data-dismiss='modal'><span>&times;</span></button>" : "") . "
-                </div>
-                <div class='modal-body'>
-                    {$config['body']}
-                </div>
-                " . ($config['footer'] ? "<div class='modal-footer justify-content-between'>{$config['footer']}</div>" : "") . "
-            </div>
-        </div>
-    </div>";
+    // 3. Build HTML
+    $html  = "<div class='modal fade{$showClass}'"
+        . " id='{$cfg['id']}'"
+        . " tabindex='-1'"
+        . " role='dialog'"
+        . " aria-labelledby='{$ariaLabelledby}'"
+        . " aria-hidden='true'"
+        . " aria-modal='true'"
+        . " {$backdropAttr}"
+        . " {$keyboardAttr}"
+        . ">";
+
+    $html .= "<div class='modal-dialog {$dialogClasses}' role='document'>";
+    $html .= "<div class='modal-content'>";
+
+    // Header
+    $html .= "<div class='modal-header {$cfg['headerClass']}'>";
+    $html .= "<h5 class='modal-title' id='{$ariaLabelledby}'>{$cfg['title']}</h5>";
+    if ($cfg['closable'])
+    {
+        $html .= "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>";
+        $html .= "<span aria-hidden='true'>&times;</span>";
+        $html .= "</button>";
+    }
+    $html .= "</div>"; // .modal-header
+
+    // Body
+    $html .= "<div class='modal-body {$cfg['bodyClass']}'>";
+    $html .= $cfg['body'];
+    $html .= "</div>"; // .modal-body
+
+    // Footer (if provided)
+    if (strlen(trim($cfg['footer'])) > 0)
+    {
+        $html .= "<div class='modal-footer justify-content-between {$cfg['footerClass']}'>";
+        $html .= $cfg['footer'];
+        $html .= "</div>"; // .modal-footer
+    }
+
+    // Close .modal-content, .modal-dialog, .modal
+    $html .= "</div></div></div>";
+
+    return $html;
 }
